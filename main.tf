f8c1b6af0793ccbafed9926342b32b4d1a5b8f4d
Kunal Mithare <kunalmithare41@gmail.coom>
2026-04-17 10:02:25 +0000

    Initial clean terraform project

---
+++ main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_website_configuration" "website_config" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = "*"
+      Action = "s3:GetObject"
+      Resource = "${aws_s3_bucket.website_bucket.arn}/*"
+    }]
+  })
+}
+
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.website_bucket.id
+  key          = "index.html"
+  source       = "index.html"
+  content_type = "text/html"
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "bucket_name" {
+  description = "Unique S3 bucket name"
+  type        = string
+}
